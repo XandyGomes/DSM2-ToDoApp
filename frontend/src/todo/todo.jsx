91edeb76e8b28fc977a586094b94bef541f678eb
XandyGomes <x3.computadores@hotmail.com>
2022-03-31 22:24:03 -0300

    Aula

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
-import PageHeader from "../template/pageHeader";
+import axios from 'axios'
 
+import PageHeader from "../template/pageHeader";
 import TodoForm from "./todoForm";
 import TodoList from "./todoList";
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component {
   constructor(props){
     super(props)
+    this.state = { description: '', list: [] }
+    this.handleChange = this.handleChange.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
   }
-  
+
+  handleChange(e){
+    this.setState({...this.state, description: e.target.value})
+  }
+
   handleAdd(){
-    console.log("Teste de adicionar")
+    const description = this.state.description
+    axios.post(URL, { description })
+      .then(resp => console.log('Este serviço está funcionando.'))
   }
 
   render() {
@@ -19,7 +30,10 @@ export default class Todo extends Component {
       <div>
         <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
         
-        <TodoForm handleAdd={this.handleAdd}/>
+        <TodoForm
+          description={this.state.description}
+          handleChange={this.handleChange}
+          handleAdd={this.handleAdd}/>
 
         <TodoList/>
 
